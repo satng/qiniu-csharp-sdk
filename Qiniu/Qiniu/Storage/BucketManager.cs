d7483e664fb05e612f977f5fb42b5a656daf94ba
jemygraw <jemygraw@gmail.com>
2015-03-09 17:58:10 +0800

    Add Qiniu csharp sdk base version.

---
+++ Qiniu/Qiniu/Storage/BucketManager.cs
@@ -0,0 +1,35 @@
+﻿using Qiniu.Common;
+using Qiniu.Http;
+using Qiniu.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qiniu.Storage
+{
+    public class BucketManager
+    {
+        private HttpManager httpManager;
+        private Mac mac;
+        public BucketManager(Mac mac)
+        {
+            this.httpManager = new HttpManager();
+            this.mac = mac;
+        }
+
+        public void stat(string bucket, string key, CompletionHandler completionHandler)
+        {
+            string url = string.Format("{0}{1}", Config.RS_HOST, statOp(bucket, key));
+            string token = Auth.createManageToken(url, null, this.mac);
+            this.httpManager.setAuthHeader(string.Format("QBox {0}", token));
+            this.httpManager.CompletionHandler = completionHandler;
+            this.httpManager.get(url);
+        }
+
+        public string statOp(string bucket, string key)
+        {
+            return string.Format("/stat/{0}", StringUtils.encodedEntry(bucket, key));
+        }
+    }
+}
---
+++ Qiniu/Qiniu/Util/Auth.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Text;
+namespace Qiniu.Util
+{
+    public class Auth
+    {
+        public static string createUploadToken(PutPolicy putPolicy, Mac mac)
+        {
+            string jsonData = putPolicy.ToString();
+            return mac.SignWithData(Encoding.UTF8.GetBytes(jsonData));
+        }
+
+        public static string createManageToken(string url, byte[] reqBody, Mac mac)
+        {
+            return mac.SignRequest(url, reqBody);
+        }
+    }
+}
