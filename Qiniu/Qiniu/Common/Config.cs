17f9864fc887c10169d2ab2c7a7ce1a21ec56777
jemygraw <jemygraw@gmail.com>
2015-08-02 16:25:12 +0800

    Add sdk zone support.

--- Qiniu/Qiniu/Common/Config.cs
+++ Qiniu/Qiniu/Common/Config.cs
@@ -9,11 +9,17 @@ namespace Qiniu.Common
         //SDK的版本号
         public const string VERSION = "1.0.0";
 
-        public const string RS_HOST = "http://rs.qiniu.com";
+        //默认上传服务器地址
+        public static string UPLOAD_HOST = "http://upload.qiniu.com";
 
-        public const string RSF_HOST = "http://rsf.qiniu.com";
+        //上传重试服务器地址
+        public static string UP_HOST = "http://up.qiniu.com";
 
-        public const string IOVIP_HOST = "http://iovip.qbox.me";
+        public static string RS_HOST = "http://rs.qiniu.com";
+
+        public static string RSF_HOST = "http://rsf.qiniu.com";
+
+        public static string IOVIP_HOST = "http://iovip.qbox.me";
 
         //分片上传块的大小，固定为4M，不可修改
         public const int BLOCK_SIZE = 4 * 1024 * 1024;
@@ -24,17 +30,28 @@ namespace Qiniu.Common
         //回复超时时间，单位秒
         public const int TIMEOUT_INTERVAL = 30;
 
-        //默认上传服务器地址
-        public static string UPLOAD_HOST = "http://upload.qiniu.com";
-
-        //上传重试服务器地址
-        public static string UP_HOST = "http://up.qiniu.com";
-
         //分片上传切片大小
         public static int CHUNK_SIZE = 2 * 1024 * 1024;
 
         //分片上传的阈值，文件超过该大小采用分片上传
         public static int PUT_THRESHOLD = 512 * 1024;
 
+        public static void UseZone0()
+        {
+            Config.UPLOAD_HOST = Zone.ZONE_0.UploadHost;
+            Config.UP_HOST = Zone.ZONE_0.UpHost;
+            Config.RS_HOST = Zone.ZONE_0.RsfHost;
+            Config.RSF_HOST = Zone.ZONE_0.RsfHost;
+            Config.IOVIP_HOST = Zone.ZONE_0.IovipHost;
+        }
+
+        public static void UseZone1()
+        {
+            Config.UPLOAD_HOST = Zone.ZONE_1.UploadHost;
+            Config.UP_HOST = Zone.ZONE_1.UpHost;
+            Config.RS_HOST = Zone.ZONE_1.RsfHost;
+            Config.RSF_HOST = Zone.ZONE_1.RsfHost;
+            Config.IOVIP_HOST = Zone.ZONE_1.IovipHost;
+        }
     }
 }
---
+++ Qiniu/Qiniu/Common/Zone.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qiniu.Common
+{
+    class Zone
+    {
+        public string UploadHost { set; get; }
+        public string UpHost { set; get; }
+        public string RsHost { set; get; }
+        public string RsfHost { set; get; }
+        public string IovipHost { set; get; }
+
+        public Zone(string uploadHost, string upHost, string rsHost, string rsfHost, string iovipHost)
+        {
+            this.UploadHost = uploadHost;
+            this.UpHost = upHost;
+            this.RsHost = rsHost;
+            this.RsfHost = rsfHost;
+            this.IovipHost = iovipHost;
+        }
+
+        public static Zone ZONE_0 = new Zone("http://upload.qiniu.com",
+                                            "http://up.qiniu.com",
+                                            "http://rs.qiniu.com",
+                                            "http://rsf.qiniu.com",
+                                            "http://iovip.qbox.me");
+
+        public static Zone ZONE_1 = new Zone("http://upload-z1.qiniu.com",
+                                            "http://up-z1.qiniu.com",
+                                            "http://rs-z1.qiniu.com",
+                                            "http://rsf-z1.qiniu.com",
+                                            "http://iovip-z1.qbox.me");
+    }
+}
